2ca11e3242271a1f688cfd7532645a29e992e1e7
PatrickRuf <54642246+PatrickRuf@users.noreply.github.com>
2021-04-19 14:56:43 -0400

    Added project with InteractML

---
+++ InteractML Keyboard Movement/Assets/Light_Control.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InteractML;
+
+public class Light_Control : MonoBehaviour
+{
+    Light Under_light;
+
+    [PullFromIMLController, Range(0, 10)]
+    public float LightIntesity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Under_light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Under_light.intensity = LightIntesity;
+    }
+}
---
+++ InteractML Keyboard Movement/Assets/Scripts/Duck_Quack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InteractML;
+
+public class Duck_Quack : MonoBehaviour
+{
+    AudioSource DuckSound;
+
+    [PullFromIMLController, Range(0, 1)]
+    public float Volume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DuckSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Volume <= 1)
+        {
+            DuckSound.volume = Volume;
+        }
+
+    }
+}
